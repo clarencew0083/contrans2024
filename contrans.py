cde5945bc528f0c5b5f077f16b79c22c84666983
Clarence Williams <clarencewilliams@Clarences-MacBook-Pro.local>
2024-09-25 12:19:49 -0400

    Added methods for accessing member data on Congress API

--- contrans.py
+++ contrans.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import os
+import dotenv
+import requests
+import json
 
 
 class contrans:
@@ -12,6 +15,8 @@ class contrans:
         expected to contain the password for the database.
         """
         self.mypassword = os.getenv('mypassword')
+        self.congresskey = os.getenv('congresskey')
+        self.newskey = os.getenv('newskey')
 
     def get_votes(self):
 
@@ -59,3 +64,31 @@ class contrans:
         url = url = 'https://voteview.com/static/data/out/members/H118_members.csv'
         members = pd.read_csv(url)
         return members
+    
+    def get_useragent(self):
+        url = 'https://httpbin.org/user-agent'
+        r = requests.get(url)
+        useragent = json.loads(r.text)['user-agent']
+        return useragent
+    
+    def make_headers(self, 
+                    email='dnh9rs@virginia.edu'):
+        useragent = self.get_useragent(),
+        headers = {
+            'User-Agent': useragent,
+            'From': email
+        }
+        return headers
+    
+    def get_bioguideIDs(self):
+        params = {'api_key': self.congresskey}
+        headers = self.make_headers()
+        root = 'https://api.congress.gov/v3/'
+        endpoint = '/member'
+        r = requests.get(root + endpoint, 
+                         params=params,
+                         )
+        bio_df = pd.json_normalize(r.json(), record_path=['members'])
+        bio_df = bio_df[['name', 'state', 'district', 'bioguideId']]
+
+        return bio_df
